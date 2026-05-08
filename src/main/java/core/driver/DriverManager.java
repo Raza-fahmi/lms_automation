fb8102cbef90e2d0bd1c3c649b3dc999aef72450
Fahmeza <89631896+Raza-fahmi@users.noreply.github.com>
2026-05-08 23:38:56 +0700

    Refactor DriverFactory to DriverManager class
    Refactored DriverFactory to DriverManager for better thread safety and management of WebDriver instances.

--- src/main/java/core/driver/DriverManager.java
+++ src/main/java/core/driver/DriverManager.java
@@ -2,30 +2,23 @@ package core.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-public class DriverFactory {
-    
-    public static WebDriver createDriver() {
-        ChromeOptions options = new ChromeOptions();
-        
-        // Deteksi apakah running di GitHub Actions
-        String ciEnv = System.getenv("CI");
-        if (ciEnv != null && ciEnv.equals("true")) {
-            // Mode headless untuk CI
-            options.addArguments("--headless=new");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--remote-allow-origins=*");
-            options.addArguments("--window-size=1920,1080");
-        } else {
-            // Mode normal untuk lokal
-            options.addArguments("--remote-allow-origins=*");
+public class DriverManager {
+
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static void setDriver(WebDriver webDriver) {
+        driver.set(webDriver);
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
         }
-        
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver(options);
     }
 }
